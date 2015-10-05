b7afaea000ed3caad533fbd3fd614833fff472d3
tomafy <yfang1@cmu.andrew.edu>
2015-10-05 01:40:23 -0400

    update method

--- ch4/Solution410.java
+++ ch4/Solution410.java
@@ -1,31 +1,27 @@
 package hw2;
 
 public class Solution410 {
-	public boolean checkSubtree(TreeNode p, TreeNode q) {
-		boolean result = false;
-		//travers the p tree to find a node whose val equals the val of q
-		if (p != null && q != null) {
-			if (p.val == q.val)
-				//Check the structure of the sub tree
-				result = checkStructure(p, q);
-			if (!result)
-				//Then check left subtree
-				result = checkStructure(p.left, q);
-			if (!result)
-				//Then check right subtree
-				result = checkStructure(p.right, q);
-		}
-		return result;
-	}
-
-	public boolean checkStructure(TreeNode p, TreeNode q) {
-		//compare each nodes
-		if (p == null)
-			return false;
-		if (q == null)
-			return true;
-		if (p.val != q.val)
-			return false;
-		return checkStructure(p.left, q.left) && checkStructure(p.right, q.right);
-	}
+    
+    public boolean checkSubtree(TreeNode p, TreeNode q) {
+        if(q==null) return true;//empty tree is subtree of any tree
+        if(p == null) return false;
+        //travers the p tree to find a node whose val equals the val of q
+        //Match the subtree
+        if (p.val == q.val&&checkStructure(p,q)){
+            return true;}
+        //continue checking whether the tree is subtree of left or right
+        return checkSubtree(p.left,q)||checkSubtree(p.right,q);
+    }
+    
+    public boolean checkStructure(TreeNode p, TreeNode q) {
+        //compare each nodes
+        if(p==null&&q==null){
+            return true;
+        }else if(p==null||q==null){
+            return false;
+        }
+        if (p.val != q.val)
+            return false;
+        return checkStructure(p.left, q.left) && checkStructure(p.right, q.right);
+    }
 }
