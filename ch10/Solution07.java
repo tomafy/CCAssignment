5f051317c331db584a002f351a08093fca4e3a0b
tomafy <yfang1@cmu.andrew.edu>
2015-10-07 15:39:02 -0400

    update

--- ch10/Solution07.java
+++ ch10/Solution07.java
@@ -1,5 +1,29 @@
 package hw3;
 
-public class Solution07 {
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
 
+public class Solution07 {
+//Use bit vector with 8 billion bits
+	long numberOfint = ((long)Integer.MAX_VALUE)+1;
+	byte[] bv = new byte[(int) (numberOfint/8)];
+	String filename = "...";
+	
+	void findNum() throws FileNotFoundException{
+		Scanner in = new Scanner(new FileReader(filename));
+		while(in.hasNextInt()){
+			int x = in.nextInt();
+			bv[x/8] |=1<<(x%8);			
+		}
+		 for(int i=0;i<bv.length;i++){
+			 for(int j = 0;j<8;j++){
+				 //retrieve  the each bit of each byte. When 0 bit is found, current value is the expected value
+				 if((bv[i]&(1<<j))==0){
+					 System.out.println(i*8+j);
+					 return;
+				 }
+			 }
+		 }
+	}
 }
--- ch10/Solution10.java
+++ ch10/Solution10.java
@@ -3,14 +3,36 @@ package hw3;
 class Node {
 	public int leftSize = 0;
 	public Node left;
-	public Node rightl;
+	public Node right;
 	public int val;
 
 	public Node(int v) {
 		val = v;
 	}
 }
-
+//use the property of BST to find the num
+//the node in the left subtree must be less than the parent and in the right subtree must be bigger than the pareant nodes
 public class Solution10 {
+public int getRank(int d, Node root){
+   if(root.val == d){
+	   return root.leftSize;  
+   }else if(d<root.val){
+	   if(root.left==null) return -1;
+	   else return getRank(d,root.left);
+   }else{
+	   if(root.right==null) return -1;
+	   else return root.leftSize+1+getRank(d,root.right);
+   }
+}
 
+public void insert(int d, Node root){
+	if(d<=root.val){
+		if(root.left != null) insert(d,root.left);
+		else root.left = new Node(d);
+		root.leftSize++;
+	}else{
+		if(root.right != null) insert(d,root.right);
+		else root.right = new Node(d);
+	}
+}
 }
